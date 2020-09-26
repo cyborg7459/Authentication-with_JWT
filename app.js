27cd9c65fdd178169c19e1c4ea661e1c8630145c
Shreyash Kumar Singh <shreyash.ksingh.cd.eee19@itbhu.ac.in>
2020-09-26 19:00:57 +0530

    Add middleware for route protection

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const authRoutes = require('./routes/authRoutes');
+const requireAuth = require('./middleware/authmiddleware');
 
 const app = express();
 app.use(express.static('public'));
@@ -24,5 +25,5 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCr
 
 // routes
 app.get('/', (req, res) => res.render('home'));
-app.get('/smoothies', (req, res) => res.render('smoothies'));
+app.get('/smoothies', requireAuth, (req, res) => res.render('smoothies'));
 app.use(authRoutes);
---
+++ middleware/authmiddleware.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken');
+
+const requireAuth = (req,res,next) => {
+    const token = req.cookies.jwt;
+    if(token) {
+        jwt.verify(token, 'Chaos isnt a pit chaos is a laddah', (err, decodedToken) => {
+            if(err) {
+                console.log(err.message);
+                res.redirect('/login');
+            }
+            else {
+                next();
+            }
+        });
+    }
+    else {
+        res.redirect('/login');
+    }
+}
+
+module.exports = requireAuth;
