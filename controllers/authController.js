349352f926fdf8efc715a6d7a950bb2b18d9758d
Shreyash Kumar Singh <shreyash.ksingh.cd.eee19@itbhu.ac.in>
2020-09-26 11:07:31 +0530

    Wrote validations

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,16 +1,53 @@
+const User = require('../models/user');
+
+const handleError = (err) => {
+    console.log(err.code);
+    let errors = {
+        email : '',
+        password : ''
+    };
+    if(err.code === 11000) {
+        errors.email = 'Entered email is already registered';
+        return errors;
+    }
+    else if(err.message.includes('user validation failed')) {
+        Object.values(err.errors).forEach(error => {
+            errors[error.path] = error.message;
+        });
+    }
+    return errors;
+}
+
 exports.signup_get = (req,res) => {
     res.render('signup');
 }
 
-exports.signup_post = (req,res) => {
-    res.send('New Signup');
+exports.signup_post = async (req,res) => {
+    const {email,password} = req.body;
+    try {
+        const user = await User.create({
+            email,
+            password
+        });
+        res.status(201).json({
+            status: 'success',
+            user
+        });
+    }
+    catch(err) {
+        const errors = handleError(err);
+        res.status(400).json({
+            status: 'failure',
+            errors
+        });
+    }
 }
 
 exports.login_get = (req,res) => {
     res.render('login');
 }
 
-exports.login_post = (req,res) => {
+exports.login_post = async (req,res) => {
     res.send('New login');
 }
 
---
+++ models/user.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const { isEmail } = require('validator');
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: [true, 'Please enter an email'],
+        unique: true,
+        lowercase: true,
+        validate: [isEmail, 'Please enter a valid email']
+    },
+    password : {
+        type: String,
+        required: [true, 'Please enter a password'],
+        minlength: [6, 'Password should be atleast 6 characters long']
+    }
+})
+
+const User = mongoose.model('user', userSchema);
+module.exports = User;
